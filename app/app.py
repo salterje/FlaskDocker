40e2d2bc4384e8930e2c7ed9eee6aeaae2ea8ac6
Jason Salter <salter.je@gmail.com>
2020-05-29 16:07:03 +0100

    Added Table of environmental values and css

--- app/app.py
+++ app/app.py
@@ -13,10 +13,12 @@ def index():
     local_time = time.asctime()
     uname = os.uname()
     actual_os = uname[3]
+    KEY1 = os.environ.get('KEY1')
+    KEY2 = os.environ.get('KEY2')
 
     return render_template('basic.html', hostname=hostname,
                             ip_address=ip_address, local_time=local_time,
-                            actual_os=actual_os)
+                            actual_os=actual_os, KEY1=KEY1, KEY2=KEY2)
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0')
